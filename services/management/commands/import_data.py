c60930140e9b1ff0668569d1331508e7a4ee41dc
RasulZT <zt.rasik@gmail.com>
2025-04-28 09:57:48 +0500

    change import data

--- services/management/commands/import_data.py
+++ services/management/commands/import_data.py
@@ -3,14 +3,15 @@ import pandas as pd
 from services.models import Parking
 
 class Command(BaseCommand):
-    help = "Импорт данных из Excel в модель Parking"
+    help = "Импорт данных из Excel в модель Parking, сохраняя ID"
 
     def handle(self, *args, **kwargs):
         df = pd.read_excel("services/management/commands/parkings_2.xlsx")
 
         created = 0
         for _, row in df.iterrows():
-            Parking.objects.create(
+            parking = Parking(
+                id=int(row["id"]),  # ⚡ установить id вручную
                 name=row["name"],
                 host=row["host"],
                 ip=row["ip"],
@@ -18,6 +19,7 @@ class Command(BaseCommand):
                 group_chat_id=row.get("group_chat_id") if pd.notna(row.get("group_chat_id")) else None,
                 language_code=row.get("language_code", "ru")
             )
+            parking.save(force_insert=True)  # ⚡ force_insert чтобы Django не искал, а создавал с данным id
             created += 1
 
         self.stdout.write(self.style.SUCCESS(f"✅ Успешно загружено {created} парковок"))
